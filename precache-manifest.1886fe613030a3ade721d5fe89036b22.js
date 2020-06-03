bf582424677e0f42c486918351eb8dd83ac6d657
Tobias Fried <friedtm@gmail.com>
2020-06-03 13:32:12 -0400

    Updates

---
+++ precache-manifest.1886fe613030a3ade721d5fe89036b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43071ce9860ed45b723d02c32b09f49e",
+    "url": "/amplifi/index.html"
+  },
+  {
+    "revision": "ae0861511f1d510fd93d",
+    "url": "/amplifi/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "1758939bd37ba7316e68",
+    "url": "/amplifi/static/js/2.2de7d536.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amplifi/static/js/2.2de7d536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0861511f1d510fd93d",
+    "url": "/amplifi/static/js/main.1dba2659.chunk.js"
+  },
+  {
+    "revision": "9f9f0c7ff7b58516d812",
+    "url": "/amplifi/static/js/runtime-main.a0098a54.js"
+  }
+]);
